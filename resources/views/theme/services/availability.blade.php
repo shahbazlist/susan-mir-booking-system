aa20685566e17d4d40a154a6fe37e735043a414b
Shahbaz Ali <shahbaz@listandsell.de>
2024-01-11 14:07:13 +0530

    first commit

--- resources/views/theme/services/availability.blade.php
+++ resources/views/theme/services/availability.blade.php
@@ -98,14 +98,13 @@
         <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLabel">Booking History</h5>
-              <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-            <div id="view_response"></div>
             {{-- Table start --}}
-            
+            <div id="view_response"></div>
             {{-- Table End --}}
         </div>
         </div>
