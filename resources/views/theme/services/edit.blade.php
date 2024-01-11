3ecc5efd96bb555e0c5b00d6c73046bfbe3de0b9
Shahbaz Ali <shahbaz@listandsell.de>
2024-01-11 10:36:59 +0530

    first commit

--- resources/views/theme/services/edit.blade.php
+++ resources/views/theme/services/edit.blade.php
@@ -81,6 +81,9 @@
 @endsection
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <script>
+  $(document).on('input', '#slot,#price', function() {
+          $(this).val($(this).val().replace(/\D/g, ''))
+      });
     $(function(){
     var dtToday = new Date();
 
